7541f7f1b9a7f3c9e895a2529e8de0e7ad208971
Kevin <kevindc39@hotmail.com>
2025-07-21 12:04:05 +0200

    Activité 5 : Corrections v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,26 @@
 import { Container, Nav } from "react-bootstrap";
+import { Link } from 'react-router';
 
 function Header() {
   return (
     <header className="bg-light border-bottom py-2">
       <Container>
-        <Nav className="justify-content-end">
-          <Nav.Link as="a" href="/">
-            Accueil
-          </Nav.Link>
-          <Nav.Link as="a" href="/categorie/vetements">
-            Vêtements
-          </Nav.Link>
-          <Nav.Link as="a" href="/categorie/chaussures">
-            Chaussures
-          </Nav.Link>
+        <Nav as="ul" className="justify-content-end mb-0">
+          <Nav.Item as="li">
+            <Nav.Link as={Link} to="/">
+              Accueil
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link as={Link} to="/categorie/vetements">
+              Vêtements
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Nav.Link as={Link} to="/categorie/chaussures">
+              Chaussures
+            </Nav.Link>
+          </Nav.Item>
         </Nav>
       </Container>
     </header>
