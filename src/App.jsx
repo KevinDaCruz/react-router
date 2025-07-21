07a5d74b752a06d6427e826bfbfaf4f758909ec6
Kevin <kevindc39@hotmail.com>
2025-07-21 11:04:59 +0200

    Activité 4

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { Route, Routes } from "react-router";
 // Import des composants/pages
 import Home from "./pages/Home";
 import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
 import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="/categorie/:category" element={<Category />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
