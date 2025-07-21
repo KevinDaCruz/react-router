c57c3b68bb41eadee7e2513a3763f56aacc549b4
Kevin <kevindc39@hotmail.com>
2025-07-21 11:28:25 +0200

    Activité 5

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer>
-      <p>Footer</p>;
+    <footer className="bg-dark text-white text-center">
+      <p>Footer</p>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,21 @@
+import { Container, Nav } from "react-bootstrap";
+
 function Header() {
   return (
-    <header>
-      <p>Header</p>
+    <header className="bg-light border-bottom py-2">
+      <Container>
+        <Nav className="justify-content-end">
+          <Nav.Link as="a" href="/">
+            Accueil
+          </Nav.Link>
+          <Nav.Link as="a" href="/vetements">
+            Vêtements
+          </Nav.Link>
+          <Nav.Link as="a" href="/chaussures">
+            Chaussures
+          </Nav.Link>
+        </Nav>
+      </Container>
     </header>
   );
 }
